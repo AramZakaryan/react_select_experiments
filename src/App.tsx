d59706281f43152941f983d44654d26cc5ef8ed9
Aram Zakaryan <aram.zakaryan@n.rich>
2024-12-10 13:33:01 +0400

    reset for react-select

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,7 @@
-import React from 'react'
-
-const Inputs = () => {
-  const onClickForm = (ev: React.MouseEvent<HTMLFormElement, MouseEvent>) => {
-    console.log('controlled: ', ev.currentTarget)
-    console.log('uncontrolled: ' /* вставить value2 */)
-  }
-  const myRef = React.useRef(null)
-  myRef.current.return(
-    <form onClick={onClickForm}>
-      <input placeholder="controlled" />
-      <input placeholder="uncontrolled" />
-      <button>Отправить заявку на кредит</button>
-    </form>,
-  )
-}
-
 export function App() {
   return (
     <>
-      <Inputs />
+      hello
     </>
   )
 }
